5895d900c39b1d1cea1f516a16643783dd4aaafe
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-19 00:07:17 +0900

    호출 순서 바뀜

--- src/main/scala/actors/User.scala
+++ src/main/scala/actors/User.scala
@@ -13,7 +13,7 @@ object User {
 	case object Terminate
 	case class PartnerName(name: String)
 }
-class User(name: String, matchManager: ActorRef) extends Actor with ActorLogging{
+class User(name: String, matchManager: ActorRef) extends Actor{
 	import User._
 	
 	override def receive: Receive = awaitConnection
@@ -21,13 +21,13 @@ class User(name: String, matchManager: ActorRef) extends Actor with ActorLogging
 	def awaitConnection: Receive = {
 		case Connected(outgoing) =>
 			println(s"[$name] Connected with server.")
-			matchManager ! Waiting(name)
 			context.become(awaitPartner(outgoing))
+			matchManager ! Waiting(name)
 	}
 	
 	def awaitPartner(outgoing: ActorRef): Receive = {
 		case Matched(chatManager, partner) =>
-			outgoing ! OutgoingMessage(partner)
+			outgoing ! OutgoingMessage(s"${partner}님과 연결되었습니다.")
 			context.become(withPartner(chatManager, outgoing))
 		case IncomingMessage("/terminate") =>
 			println(s"User $name terminate.")
@@ -44,10 +44,12 @@ class User(name: String, matchManager: ActorRef) extends Actor with ActorLogging
 		case IncomingMessage(msg) =>
 			println(s"[${name}] Sent a message: $msg")
 			chatManager ! ChatManager.ChatMessage(s"$name: $msg")
-		case ChatManager.ChatMessage(msg) => outgoing ! OutgoingMessage(msg)
+		case ChatManager.ChatMessage(msg) =>
+			println(s"[${name}] Got a message: $msg")
+			outgoing ! OutgoingMessage(msg)
 		case ChatManager.ChatTerminated =>
+			context.become(awaitPartner(outgoing))
 			outgoing ! OutgoingMessage("/terminated")
 			matchManager ! Waiting(name)
-			context.become(awaitPartner(outgoing))
 	}
 }
