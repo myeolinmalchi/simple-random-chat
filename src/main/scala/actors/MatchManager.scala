eb2fdd48046b93ff318c1c9f88b47abc8ea009cd
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-04-20 01:50:45 +0900

    MODIFY: sender() 참조할거면 임시 변수에 따로 담아둬야함

--- src/main/scala/actors/MatchManager.scala
+++ src/main/scala/actors/MatchManager.scala
@@ -4,28 +4,24 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 
 class MatchManager extends Actor {
 	
-	override def receive: Receive = noWaiting(List())
+	override def receive: Receive = noWaiting
 	
-	def noWaiting(chatList: List[ActorRef]): Receive = {
+	def noWaiting: Receive = {
 		case User.Waiting(name) =>
-			context.become(withWaiting(chatList, sender(), name))
-		case Terminated(chat) =>
-			println(s"Chatroom $chat terminated.")
-			val newList = chatList filterNot chat.==
-			context.become(noWaiting(newList))
+			println(s"$name is waiting.")
+			context.watch(sender())
+			context.become(withWaiting(sender(), name))
 	}
 	
-	def withWaiting(chatList: List[ActorRef], waitingUser: ActorRef, waitingUserName: String): Receive = {
+	def withWaiting(waitingUser: ActorRef, waitingUserName: String): Receive = {
 		case User.Waiting(name) =>
 			println(s"User $waitingUserName and $name are matched.")
-			val newChat = context.actorOf(Props(new ChatManager(waitingUser, sender())))
+			val newUser = sender()
+			val newChat = context.actorOf(Props(new ChatManager(waitingUser, newUser)))
 			waitingUser ! User.Matched(newChat, name)
-			sender() ! User.Matched(newChat, waitingUserName)
-			context.watch(newChat)
-			context.become(noWaiting(newChat::chatList))
-		case Terminated(chat) =>
-			println(s"Chatroom $chat terminated.")
-			val newList = chatList filterNot chat.==
-			context.become(withWaiting(newList, waitingUser, waitingUserName))
+			newUser ! User.Matched(newChat, waitingUserName)
+			context.unwatch(waitingUser)
+			context.become(noWaiting)
+		case Terminated(_) => context.become(noWaiting)
 	}
 }
